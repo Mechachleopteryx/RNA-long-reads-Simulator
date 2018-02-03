64fa766bb179259c0a5338f7704f1eec078d42aa
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2018-02-03 14:36:48 +0100

    Adding a par to optionally add a path to FS

--- RNACreator.py
+++ RNACreator.py
@@ -113,7 +113,7 @@ def main():
 	parser.add_argument('-r',	action="store",	dest="genomeRefPath",	type=str,	default = "",	help="Path to the reference genome file")
 	parser.add_argument('-c', action="store", dest="coverage",	type=str,	default = "1",	help="An integer that represents the desired coverage (default=1)")
 	parser.add_argument('-o', action="store", dest="outputDirPath",	type=str,	default = ".",	help="Path to the output directory (default: .)")
-	parser.add_argument('-fsPath', action="store", dest="fluxSimulatorBin",	type=str,	default = "flux-simulator",	help="Path to flux-simulator binary (default: flux-simulator (the bin is in your path))")
+	parser.add_argument('--fsPath', action="store", dest="fluxSimulatorBin",	type=str,	default = "flux-simulator",	help="Path to flux-simulator binary (default: flux-simulator (the bin is in your path))")
 	parser.add_argument('--version', action='version', version='%(prog)s 0.0.1')
 
 	# ------------------------------------------------------------------------
